2e843f2e3637f1b30cae50d53734d7781720557e
sxm92 <juanjosequirozm@hotmail.cl>
2019-02-05 15:41:25 -0300

    modificado

--- wsgi.py
+++ wsgi.py
@@ -1,15 +1,15 @@
 from flask import Flask
 from flask import render_template
 
-app = Flask(__name__)
+application = Flask(__name__)
 
-@app.route("/")
+@application.route("/")
 def home():
     return render_template('index.html')
 
-@app.route('/pmu/<datapmu>')
+@application.route('/pmu/<datapmu>')
 def PmuDatas(datapmu = 0):
     return render_template('web.html',dato=datapmu)
 
 if __name__ == "__main__":
-    app.run(debug = True)
+    application.run(debug = True)
